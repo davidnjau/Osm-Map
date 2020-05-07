2acc5d390246121b35bd8a243e6567297f0a5119
Dave <davidnjau21@gmail.com>
2020-05-07 10:14:11 +0300

    feat(core): Preparing to populate the recyclerView with coordinates

--- app/src/main/java/com/centafrique/openstreetmap/CoordinatesAdapter.kt
+++ app/src/main/java/com/centafrique/openstreetmap/CoordinatesAdapter.kt
@@ -3,11 +3,12 @@ package com.centafrique.openstreetmap
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class CoordinatesAdapter(private val context: Context,
-                         private val LatitudesList: ArrayList<Float>,
-                         private val LongitudesList: ArrayList<Float>)
+                         private val LatitudesList: ArrayList<Double>,
+                         private val LongitudesList: ArrayList<Double>)
     : RecyclerView.Adapter<CoordinatesAdapter.CoordinatesViewHolder>(){
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -27,13 +28,26 @@ class CoordinatesAdapter(private val context: Context,
     class CoordinatesViewHolder(inflater: LayoutInflater, parent: ViewGroup):
             RecyclerView.ViewHolder(inflater.inflate(R.layout.activity_main, parent, false)){
 
+        private var tvLat : TextView? = null
+        private var tvLong : TextView? = null
+
         init {
+
+            tvLat = itemView.findViewById(R.id.tvLat)
+            tvLong = itemView.findViewById(R.id.tvLong)
+
 
         }
 
         fun AssignValues(txtLat: String, txtLong: String, context: Context){
 
+            tvLat?.text = txtLat
+            tvLong?.text = txtLong
 
+            itemView.setOnClickListener {
+
+
+            }
 
         }
 
--- app/src/main/java/com/centafrique/openstreetmap/MainActivity.kt
+++ app/src/main/java/com/centafrique/openstreetmap/MainActivity.kt
@@ -2,17 +2,51 @@ package com.centafrique.openstreetmap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView : RecyclerView
 
+
+    private lateinit var coordinatesAdapter: CoordinatesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         recyclerView = findViewById(R.id.recyclerView)
+
+        val LatitudesList = arrayListOf<Double>(
+            -1.563813,
+            -1.549261,
+            -1.620816,
+            0.444393,
+            1.685609,
+            2.036985,
+            -0.873928,
+            0.609181
+        )
+
+        val LongitudesList = arrayListOf<Double>(
+            36.841117,
+            36.751319,
+            34.354268,
+            35.749396,
+            37.858565,
+            36.166675,
+            35.419622,
+            35.760164
+        )
+
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,
+            false)
+
+//        coordinatesAdapter = CoordinatesAdapter(this, LatitudesList, LongitudesList)
+//
+//        recyclerView.adapter = coordinatesAdapter
 
 
     }
